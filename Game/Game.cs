d8a8d43be49ccb0657f46a56e097eb0508f9a612
Adam <aspinov3@gmail.com>
2023-02-10 16:24:13 +0300

    Добавьте файлы проекта.

---
+++ Game/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Heroes
+{
+    internal class Game
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
